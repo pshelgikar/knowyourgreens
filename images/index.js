d431df52c2fdbd037d5d2ba546e48df724299c8e
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-08-08 08:32:46 +0000

    Background image on varietals page

---
+++ images/index.js
@@ -0,0 +1,119 @@
+import aglaonema from './../images/aglaonema.jpeg'
+import airplant from './../images/air-plant.jpeg'
+import alocasia from './../images/alocasia.jpeg'
+import aloe from './../images/aloe.jpeg'
+import anthurium from './../images/anthurium.jpeg'
+import aralia from './../images/aralia.jpeg'
+import arrowheadplant from './../images/arrowhead-plant.jpeg'
+import begonia from './../images/begonia.jpeg'
+import birdofparadise from './../images/bird-of-paradise.jpeg'
+import bromeliad from './../images/bromeliad.jpeg'
+import burgundyrubbertree from './../images/burgundy-rubber-tree.jpeg'
+import cactus from './../images/cactus.jpeg'
+import calathea from './../images/calathea.jpeg'
+import carnivorousplant from './../images/carnivorous-plant.jpeg'
+import croton from './../images/croton.jpeg'
+import dieffenbachia from './../images/dieffenbachia.jpeg'
+import dracaena from './../images/dracaena.jpeg'
+import dwarfbananatree from './../images/dwarf-banana-tree.jpeg'
+import fatsia from './../images/fatsia.jpeg'
+import fern from './../images/fern.jpeg'
+import ficus from './../images/ficus.jpeg'
+import fiddleleaffig from './../images/fiddle-leaf-fig.jpeg'
+import fittonia from './../images/fittonia.jpeg'
+import herbs from './../images/herbs.jpeg'
+import homalomena from './../images/homalomena.jpeg'
+import hoya from './../images/hoya.jpeg'
+import hypoestes from './../images/hypoestes.jpeg'
+import ivy from './../images/ivy.jpeg'
+import jade from './../images/jade.jpeg'
+import lipstickplant from './../images/lipstick-plant.jpeg'
+import luckybamboo from './../images/lucky-bamboo.jpeg'
+import moneytree from './../images/money-tree.jpeg'
+import monstera from './../images/monstera.jpeg'
+import norfolkpine from './../images/norfolk-pine.jpeg'
+import orchid from './../images/orchid.jpeg'
+import oxalis from './../images/oxalis.jpeg'
+import palm from './../images/palm.jpeg'
+import peacelily from './../images/peace-lily.jpeg'
+import peperomia from './../images/peperomia.jpeg'
+import philodendron from './../images/philodendron.jpeg'
+import pilea from './../images/pilea.jpeg'
+import poinsettia from './../images/poinsettia.jpeg'
+import ponytailpalm from './../images/ponytail-palm.jpeg'
+import pothos from './../images/pothos.jpeg'
+import prayerplant from './../images/prayer-plant.jpeg'
+import purplewaffle from './../images/purple-waffle.jpeg'
+import rhoeooyster from './../images/rhoeooyster.jpeg'
+import rhaphidophora from './../images/rhaphidophora.jpeg'
+import sansevieria from './../images/sansevieria.jpeg'
+import schefflera from './../images/schefflera.jpeg'
+import spiderplant from './../images/spider-plant.jpeg'
+import stromanthe from './../images/stromanthe.jpeg'
+import succulent from './../images/succulent.jpeg'
+import tradescantiazebrina from './../images/tradescantia-zebrina.jpeg'
+import vegetableplant from './../images/vegetable-plant.jpeg'
+import yuccacane from './../images/yucca-cane.jpeg'
+import zebraplant from './../images/zebra-plant.jpeg'
+import zzplant from './../images/zz-plant.jpeg'
+
+export default {
+    zzplant,
+    zebraplant,
+    yuccacane,
+    vegetableplant,
+    tradescantiazebrina,
+    succulent,
+    stromanthe,
+    spiderplant,
+    schefflera,
+    sansevieria,
+    rhoeooyster,
+    rhaphidophora,
+    purplewaffle,
+    prayerplant,
+    pothos,
+    ponytailpalm,
+    poinsettia,
+    pilea,
+    philodendron,
+    peperomia,
+    peacelily,
+    palm,
+    oxalis,
+    orchid,
+    norfolkpine,
+    monstera,
+    moneytree,
+    luckybamboo,
+    lipstickplant,
+    jade,
+    ivy,
+    hypoestes,
+    hoya,
+    homalomena,
+    herbs,
+    fittonia,
+    fiddleleaffig,
+    ficus,
+    fern,
+    fatsia,
+    dwarfbananatree,
+    dracaena,
+    dieffenbachia,
+    croton,
+    carnivorousplant,
+    calathea,
+    cactus,
+    burgundyrubbertree,
+    bromeliad,
+    birdofparadise,
+    begonia,
+    arrowheadplant,
+    aralia,
+    anthurium,
+    aloe,
+    alocasia,
+    airplant,
+    aglaonema
+};
